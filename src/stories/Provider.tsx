a3f96b10d6b69225f3dab6d1fae9dfcdd7bcdebc
sho <sskmy1024.y@gmail.com>
2020-03-09 19:40:45 +0900

    add react redux

--- src/stories/Provider.tsx
+++ src/stories/Provider.tsx
@@ -1,37 +1,30 @@
 import React from 'react'
-// import configureStore from 'redux-mock-store'
-// import thunkMiddleware from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+import thunkMiddleware from 'redux-thunk'
 
-// import { Provider } from 'react-redux'
-// import { RootState } from 'modules'
-// import { initialState } from 'modules'
-// import { library } from '@fortawesome/fontawesome-svg-core'
+import { Provider } from 'react-redux'
+import { State as RootState } from 'reducer'
+import { initialState } from 'reducer'
 
-// library.add(fab, fas)
+export default function StorybookProvider({
+  story,
+  state: stateProp
+}: {
+  story: React.ReactChild | (() => React.ReactChild)
+  state?: RootState | (() => RootState)
+}) {
+  const children = typeof story === 'function' ? story() : story
 
-// export default function StorybookProvider({
-//   story,
-//   state: stateProp
-// }: {
-//   story: React.ReactChild | (() => React.ReactChild)
-//   state?: RootState | (() => RootState)
-// }) {
-//   const children = typeof story === 'function' ? story() : story
+  const mockStore = configureStore([thunkMiddleware])
+  const state = {
+    ...initialState,
+    ...(typeof stateProp === 'function' ? stateProp() : stateProp)
+  }
+  const store = mockStore(state)
 
-//   const mockStore = configureStore([thunkMiddleware])
-//   const state = {
-//     ...initialState,
-//     ...(typeof stateProp === 'function' ? stateProp() : stateProp)
-//   }
-//   const store = mockStore(state)
-
-//   return (
-//     <Provider store={store} key={Math.random()}>
-//       {children}
-//     </Provider>
-//   )
-// }
-
-export default function Provider() {
-  return <div />
+  return (
+    <Provider store={store} key={Math.random()}>
+      {children}
+    </Provider>
+  )
 }
