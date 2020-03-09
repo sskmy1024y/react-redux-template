620ad186e3033d53506b5c97735f0bb2845f9e7b
sho <sskmy1024.y@gmail.com>
2020-03-09 10:32:32 +0900

    add dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,41 @@
 const path = require("path");
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.tsx",
-  output: {
-    path: `${__dirname}/public`,
-    filename: "bundle.js"
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)?$/,
-        include: path.resolve(__dirname, "src"),
-        use: "ts-loader"
-      }
-    ]
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  }
-};
+const outputPath = path.resolve(__dirname, "public");
+
+function webpackConfig(rawEnv, optionArg) {
+  return {
+    mode: "development",
+    devtool: "eval-source-map",
+    entry: "./src/index.tsx",
+    output: {
+      path: outputPath,
+      filename: "bundle.js"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)?$/,
+          include: path.resolve(__dirname, "src"),
+          use: "ts-loader"
+        },
+        {
+          test: /\.(js|jsx)?$/,
+          include: path.resolve(__dirname, "src"),
+          use: "babel-loader"
+        }
+      ]
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    devServer: {
+      port: 3000,
+      contentBase: outputPath,
+      compress: true,
+      // disableHostCheck ... localhost外からのアクセスを許可する
+      disableHostCheck: true
+    }
+  };
+}
+
+module.exports = webpackConfig;
